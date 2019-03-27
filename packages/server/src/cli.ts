bda4ad3ace231e369f20565b1787332e860fd04a
John McCambridge <jmccambridge@coder.com>
2019-03-27 15:30:44 -0500

    Import getRecentRelease to cli

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -8,6 +8,7 @@ import * as os from "os";
 import * as path from "path";
 import * as WebSocket from "ws";
 import { buildDir, cacheHome, dataHome, isCli, serveStatic } from "./constants";
+import { getRecentRelease } from "./updater";
 import { setup as setupNativeModules } from "./modules";
 import { createApp } from "./server";
 import { forkModule, requireFork, requireModule } from "./vscode/bootstrapFork";
@@ -137,6 +138,10 @@ if (isCli) {
 			logger.error(`Failed to read certificate key: ${ex.message}`);
 			process.exit(1);
 		}
+	}
+
+	if (process.env.VERSION !== "development") {
+		await getRecentRelease().then((release) => console.log(release));
 	}
 
 	logger.info(`\u001B[1mcode-server ${process.env.VERSION ? `v${process.env.VERSION}` : "development"}`);
