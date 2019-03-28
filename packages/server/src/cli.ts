0a9711ca1ef148678b8fe0f8c30d8457b5288810
John McCambridge <jmccambridge@coder.com>
2019-03-28 15:05:28 -0500

    Remove compareVersions call, simplify version checking

--- packages/server/src/cli.ts
+++ packages/server/src/cli.ts
@@ -8,7 +8,7 @@ import * as os from "os";
 import * as path from "path";
 import * as WebSocket from "ws";
 import { buildDir, cacheHome, dataHome, isCli, serveStatic } from "./constants";
-import { getRecentRelease, compareVersions } from "./updater";
+import { getRecentRelease } from "./updater";
 import { setup as setupNativeModules } from "./modules";
 import { createApp } from "./server";
 import { forkModule, requireFork, requireModule } from "./vscode/bootstrapFork";
@@ -140,25 +140,16 @@ if (isCli) {
 		}
 	}
 
-	let newestVersion = "";
-	if (process.env.VERSION) {
+	if (isCli) {
 		let currentVersion = process.env.VERSION;
 		const recentRelease = await getRecentRelease();
 
-		if (compareVersions(currentVersion, recentRelease)! <= 1) {
-			newestVersion = recentRelease;
-		} else {
-			newestVersion = currentVersion;
+		if (recentRelease !== currentVersion) {
+			logger.warn(`New version (\u001B[1m${recentRelease}\u001B[0m) available https://github.com/codercom/code-server/releases/latest`);
 		}
 	}
-	const isDev = process.env.NODE_ENV !== "production";
-	if (newestVersion === process.env.VERSION || isDev) {
-		logger.info("You are using the latest version");
-	} else {
-		logger.warn(`New version (\u001B[1m${newestVersion}) available https://github.com/codercom/code-server/releases`);
-	}
-	logger.info(`\u001B[1mcode-server ${process.env.VERSION ? `v${process.env.VERSION}` : "development"}`);
 	// TODO: fill in appropriate doc url
+	logger.info(`\u001B[1mcode-server ${process.env.VERSION ? `v${process.env.VERSION}` : "development"}`);
 	logger.info("Additional documentation: http://github.com/codercom/code-server");
 	logger.info("Initializing", field("data-dir", dataDir), field("working-dir", workingDir), field("log-dir", logDir));
 	const sharedProcess = new SharedProcess(dataDir, builtInExtensionsDir);
